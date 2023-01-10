32a9be55be8e5b45ded2996b0bbdfe0bde6e4c54
Alefe Araujo <alefe_sk8@hotmail.com>
2023-01-10 14:40:49 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import "reflect-metadata";
 import express from 'express';
 import swaggerUi from "swagger-ui-express";
 
-import { router } from './routes';
 import swaggerFile from "./swagger.json";
+import { router } from './routes';
 
 import "./shared/container";
 
